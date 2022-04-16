5798e21f0022ff24191ecb374d7aae243d28c3c9
Arek <ogryzek.arkadiusz@gmail.com>
2022-04-16 21:21:13 +0200

    Insertion sort

---
+++ algorithms/counting_sort.py
@@ -0,0 +1,32 @@
+to_sort = [52,63,12,6,631,6,24,637,64,421,74,124,0,-5,523,-10,-529]
+
+def counting_sort(arr:list): # It seems like O(3n) => O(n) but it can get very bad quickly if numbers are big(or very small)
+                             # Aswell without negatives it should go so much faster
+
+
+    positive_list = [0] * (max(to_sort)+1)
+    negative_list = [0] * (-1*(min(to_sort)-1))
+    final_positive = []
+    final_negative = []
+
+
+    for i in to_sort: # At every array index add 1 if value in to_sort exists
+        if i < 0:
+            ti = -i
+            negative_list[ti] += 1
+        else:
+            positive_list[i] += 1
+    
+
+    # For every index add to final_arr that amount of that index number of how big number at that index is 
+    # if arr[23] = 3 then add 23 23 23 and same for negatives
+    for inx,i in enumerate(positive_list):
+            final_positive.append(i*[inx])
+    for inx,i in enumerate(negative_list):
+            final_negative.append(i*[-inx])
+    
+
+    final_negative.reverse() # Reverse negatives because we were counting them as positive [1, 5, 10] for pos will be as [-10, -5, -1]
+    return [num for sublist in final_negative + final_positive for num in sublist] # Flatten the list of lists
+
+print(counting_sort(to_sort))
--- algorithms/insertion_sort.py
+++ algorithms/insertion_sort.py
@@ -1,6 +1,6 @@
 to_sort = [52,63,12,6,631,6,24,637,64,421,74,124,0,-5,523,-10,-529]
 
-def insertion_sort(arr:list):
+def insertion_sort(arr:list): # Avg O(n^2)
     n = len(arr)
     for i in range(1,n):
         key = arr[i]
@@ -12,32 +12,3 @@ def insertion_sort(arr:list):
 
 insertion_sort(to_sort)
 print(to_sort)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-def insertion_sort(arr:list):
-    n = len(arr)
-    for i in range(1,n):
-        key = arr[i]
-        j = i - 1
-        while j >= 0 and key < arr[j]: # find value j that is minimally bigger then key,
-            arr[j+1] = arr[j]          # put that j at j+1 and then at j put key (a[j+1] = a[j] because we first subtract 1)
-            j = j - 1
-        arr[j + 1] = key # almost like arr[j]
