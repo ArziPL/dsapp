3fcc9440593798596355db00c54cc043d77b978d
Arek <ogryzek.arkadiusz@gmail.com>
2022-04-17 23:38:06 +0200

    Quick sort

---
+++ algorithms/quick_sort.py
@@ -0,0 +1,20 @@
+import math
+
+to_sort = [52,63,12,6,631,6,24,637,64,421,74,124,0,-5,523,-10,-529]
+
+def quick_sort(arr:list):
+    if len(arr) <= 1:
+        return arr
+    else:
+        left_side = []
+        right_side = []
+        pivot = arr[math.floor((len(arr)-1)/2)]
+        
+        for i in arr:
+            if i < pivot:
+                left_side.append(i)
+            elif i > pivot:
+                right_side.append(i)
+        return quick_sort(left_side) + [pivot] + quick_sort(right_side)
+
+print(quick_sort(to_sort))
